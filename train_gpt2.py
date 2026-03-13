ec2cc2cc4bf883d209d20b58062d1c634f67c3fb
sfhbarnett <sfhbarnett91@gmail.com>
2026-03-13 13:05:15 +0000

    feat: introduce fused adam, weight decay certain params

--- train_gpt2.py
+++ train_gpt2.py
@@ -174,6 +174,28 @@ class GPT(nn.Module):
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
         return logits, loss
+    
+    def configure_optimizers(self, weight_decay, learning_rate, device):
+        import inspect
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay':weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device
+        print(f'using fused AdamW: {use_fused}')
+        optimizer = torch.optim.AdamW(optim_groups, lr = learning_rate, betas=(0.9, 0.95), eps=1e-6, fused=use_fused)
+        return optimizer
         
 
 import os
@@ -237,8 +259,28 @@ model = GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 model = torch.compile(model)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9,0.95), eps=1e-8)
-for i in range(50):
+max_lr = 6e-4
+min_lr = max_lr*0.1
+warmup_steps = 10
+max_steps = 50
+
+def get_lr(it):
+    # linear warmup
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    #if it > lr_decay_iters, return min
+    if it > max_steps:
+        return min_lr
+    # in between, use cosine decay down to min
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coefficient goes from 1 to 0
+    return min_lr + coeff * (max_lr - min_lr)
+
+
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
+
+for step in range(max_steps):
     t0 = time.time()
     x,y = train_loader.next_batch()
     x = x.to(device)
@@ -248,12 +290,16 @@ for i in range(50):
         logits, loss = model(x, y)
     loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    # determine lr and update
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     optimizer.step()
     torch.cuda.synchronize()
     t1 = time.time()
     dt = (t1-t0) * 1000
     tok_per_sec = (train_loader.B * train_loader.T ) / (t1-t0)
-    print(f"step: {i} | loss: {loss.item():.6f} | norm: {norm:.4f} | dt: {dt:.2f},s | tok/sec: {tok_per_sec:.2f}")
+    print(f"step: {step:4d} | loss: {loss.item():.6f} | lr: {lr:.4e}| norm: {norm:.4f} | dt: {dt:.2f},s | tok/sec: {tok_per_sec:.2f}")
 
 
 import sys
